b6ce6a0c0cb8e1de6c922eaa7e9d6440189e608e
Pablo Zaidenvoren <pablozaiden@users.noreply.github.com>
2025-09-30 15:44:01 -0400

    shell

--- devcontainer-neovim-run.sh
+++ devcontainer-neovim-run.sh
@@ -4,4 +4,18 @@
 
 SOURCE_DIR="$(pwd)"
 
-devcontainer exec --workspace-folder "$SOURCE_DIR" nvim .
+COMMAND="nvim ."
+
+# if the first argument is --help or -h, show help message
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+  echo "Usage: $0 [shell]"
+  echo "If 'shell' is provided as an argument, it will open a shell instead of neovim."
+  exit 0
+fi
+
+# if the first argument is "shell", change the command to zsh
+if [ "$1" == "shell" ]; then
+  COMMAND="zsh"
+fi
+
+devcontainer exec --workspace-folder "$SOURCE_DIR" $COMMAND
